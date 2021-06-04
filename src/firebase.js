9f07d16a0be1562b66259ce824163a0db4749a44
Jeffrey Platt <JSPlatt@gmail.com>
2021-06-04 13:53:05 -0400

    initial setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDch6EW4Fn3mvK-fEUcCEslHIXxqw4h2VQ",
+    authDomain: "clone-e15f0.firebaseapp.com",
+    projectId: "clone-e15f0",
+    storageBucket: "clone-e15f0.appspot.com",
+    messagingSenderId: "965777401330",
+    appId: "1:965777401330:web:018629f37c5d42c878b76b",
+    measurementId: "G-7RCE0Q80QG"
+  };
