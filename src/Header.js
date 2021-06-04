32d15698957a753085a32a9c813a0abdf8aa4190
Jeffrey Platt <JSPlatt@gmail.com>
2021-06-04 14:41:08 -0400

    style started

--- src/Header.js
+++ src/Header.js
@@ -18,13 +18,13 @@ function Header() {
 
             <div className="header__nav">
                 <div className="header__option">
-                    <span className="header__optionLineOne">Hello Jeff </span>
+                    <span className="header__optionLineOne">Hello Jeff</span>
                     <span
                     className="header__optionLineTwo">Sign In</span>
                 </div>
 
                 <div className="header__option">
-                <span className="header__optionLineOne">Returns & </span>
+                <span className="header__optionLineOne">Returns& </span>
                     <span
                     className="header__optionLineTwo">Orders</span>
                 </div>
