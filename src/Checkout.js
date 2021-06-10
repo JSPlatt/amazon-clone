22f3d0c439a9d8a9943c2fef6252b78b19e061f7
Jeffrey Platt <JSPlatt@gmail.com>
2021-06-10 10:25:08 -0400

    router links added

--- src/Checkout.js
+++ src/Checkout.js
@@ -10,11 +10,17 @@ function Checkout() {
             />
              <div>
                 <h2 className="checkout__title">Your shopping Basket</h2>
+                {/* BasketItem */}
+                {/* BasketItem */}
+                {/* BasketItem */}
+                {/* BasketItem */}
+                {/* BasketItem */}
              </div>
             </div>
 
             <div className="checkout__right">
-               
+               <h2>The subtotal will go here</h2>
+                
             </div>
         </div>
     )
