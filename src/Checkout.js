94f69b7274e38ee06f85f2e3763ac4bed11e2c7c
Jeffrey Platt <JSPlatt@gmail.com>
2021-06-14 13:59:30 -0400

    checkoutproduct started

--- src/Checkout.js
+++ src/Checkout.js
@@ -11,11 +11,13 @@ function Checkout() {
             />
              <div>
                 <h2 className="checkout__title">Your shopping Basket</h2>
-                {/* BasketItem */}
-                {/* BasketItem */}
-                {/* BasketItem */}
-                {/* BasketItem */}
-                {/* BasketItem */}
+                
+                {/* CheckoutProduct */}
+                {/* CheckoutProduct */}
+                {/* CheckoutProduct */}
+                {/* CheckoutProduct */}
+                {/* CheckoutProduct */}
+                
              </div>
             </div>
 
