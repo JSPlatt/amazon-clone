482437e7d258215499fe3dc2aeab854c75e5df29
Jeffrey Platt <jsplatt@gmail.com>
2021-06-18 13:04:32 -0400

    sign in form started

--- src/Login.js
+++ src/Login.js
@@ -10,6 +10,20 @@ function Login() {
                 className='login__logo' 
                 src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' />
             </Link>
+
+            <div className='login__container'>
+                <h1>Sign-In</h1>
+
+                <form>
+                    <h5>E-mail</h5>
+                    <input type='text'/>
+
+                    <h5>Password</h5>
+                    <input type='password'/>
+
+                    <button>Sign In</button>
+                </form>
+            </div>
         </div>
     )
 }
