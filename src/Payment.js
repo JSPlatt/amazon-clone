87f68acf092238105e9bf635c07747f287880d2a
Jeffrey Platt <jsplatt@gmail.com>
2021-06-21 10:57:06 -0400

    delivery address div

--- src/Payment.js
+++ src/Payment.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import './Payment.css'
 
 function Payment() {
     return (
-        <div>
-            
+        <div className='payment'>
+            <div className='payment__container'>
+                {/* Payment Section - delivery adderss */}
+                <div className='payment__section'>
+                    <div className='payment__title'>
+                        <h3>Delivery Address</h3>
+                    </div>
+                </div>
+                {/* Payment section - Review items */}
+                <div className='payment__section'>
+                    
+                </div>
+                {/* Payment section - payment method */}
+                <div className='payment__section'>
+                    
+                </div>
+            </div>
         </div>
     )
 }
